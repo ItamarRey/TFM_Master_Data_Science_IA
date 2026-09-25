022ab8696a30a0dce323576b803a49af0e405160
ItamarRey <159778507+ItamarRey@users.noreply.github.com>
2026-09-25 12:01:29 +0100

    feat(modeling): configura __init__.py para el experimento

--- src/padel_pricing/modeling/__init__.py
+++ src/padel_pricing/modeling/__init__.py
@@ -1,1 +1,22 @@
 """Entrenamiento, evaluación y predicción de ocupación."""
+
+from padel_pricing.modeling.dataset import (
+    CATEGORICAL_FEATURES,
+    MODEL_FEATURES,
+    NUMERIC_FEATURES,
+    TARGET_COLUMN,
+    prepare_modeling_data,
+    temporal_train_test_split,
+)
+from padel_pricing.modeling.experiment import run_experiment, select_deployable_model
+
+__all__ = [
+    "CATEGORICAL_FEATURES",
+    "MODEL_FEATURES",
+    "NUMERIC_FEATURES",
+    "TARGET_COLUMN",
+    "prepare_modeling_data",
+    "run_experiment",
+    "select_deployable_model",
+    "temporal_train_test_split",
+]
