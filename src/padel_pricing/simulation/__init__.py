76f9ad33852e05dd0cdc41ecc5a444b141669a16
ItamarRey <159778507+ItamarRey@users.noreply.github.com>
2026-09-25 11:01:29 +0100

    feat(simulation): implementar __init__.py

--- src/padel_pricing/simulation/__init__.py
+++ src/padel_pricing/simulation/__init__.py
@@ -1,1 +1,12 @@
 """Generación de datos sintéticos reproducibles."""
+
+from padel_pricing.simulation.config import SimulationConfig, load_simulation_config
+from padel_pricing.simulation.generator import generate_operational_data
+from padel_pricing.simulation.weather import fetch_weather
+
+__all__ = [
+    "SimulationConfig",
+    "fetch_weather",
+    "generate_operational_data",
+    "load_simulation_config",
+]
